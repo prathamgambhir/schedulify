983cb505c9960bb31fea9f25a5390772bf5d1f69
Pratham Gambhir <prathamgambhir0307@gmail.com>
2026-01-18 16:07:40 +0530

    fix : middleware size optimization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,40 +1,34 @@
-// export { auth as middleware } from "@/auth"
+import { NextRequest, NextResponse } from "next/server";
 
-import { auth } from "@/auth"
-import { NextRequest, NextResponse } from "next/server"
+const PROTECTED_PATHS = ["/dashboard", "/events", "/availability", "/meetings"];
 
-const PROTCTED_PATHS = ["/dashboard", "/events", "availability", "meetings"]
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const token =
+    req.cookies.get("authjs.session-token")?.value ||
+    req.cookies.get("__Secure-authjs.session-token")?.value;
 
-export default auth( async(req: NextRequest) => {
-    const { pathname }= req.nextUrl;    
-    const session = await auth();
-    
-    const isProtected = PROTCTED_PATHS.some((path) => pathname.startsWith(path))
-    
-    if(isProtected && !session?.user){
-        const loginUrl = new URL("/login", req.nextUrl.origin);
-        loginUrl.searchParams.set("/callbackurl", req.nextUrl.pathname);
-        return NextResponse.redirect(loginUrl);
-    }
-    
-    if(pathname === "/login" && session?.user){
-        return NextResponse.redirect(new URL("/dashboard", req.nextUrl.origin));
-    }
-    
-    if(pathname ===  "/signup" && session?.user){
-        return NextResponse.redirect(new URL("/dashboard", req.nextUrl.origin));
-    }
-    
-    return NextResponse.next()
-})
+  const isProtected = PROTECTED_PATHS.some((path) => pathname.startsWith(path));
+
+  if (isProtected && !token) {
+    const loginUrl = new URL("/login", req.nextUrl.origin);
+    loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  if ((pathname === "/login" || pathname === "/signup") && token) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl.origin));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-    matcher: 
-    [
-        "/dashboard/:path*", 
-        "/events/:path*", 
-        "/availability/:path*", 
-        "/meetings/:path*", 
-        "/((?!api|_next/static|_next/image|.*\\.png$).*)"
-    ]
-}
+  matcher: [
+    "/dashboard/:path*",
+    "/events/:path*",
+    "/availability/:path*",
+    "/meetings/:path*",
+    "/((?!api|_next/static|_next/image|.*\\.png$).*)",
+  ],
+};
